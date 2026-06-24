5dd5a481be1078042ec598a2cb635d57a2b3ded5
iw-portal <portal@inclusiveworld.org>
2026-06-24 11:08:07 -0700

    change KEY

--- website/lib/supabaseAdmin.js
+++ website/lib/supabaseAdmin.js
@@ -2,9 +2,7 @@ import { createClient } from "@supabase/supabase-js";
 
 const SUPABASE_URL =
   process.env.SUPABASE_URL || "https://rvcronfsjghrpskywlwh.supabase.co";
-const SUPABASE_SERVICE_ROLE_KEY =
-  process.env.SUPABASE_SERVICE_ROLE_KEY ||
-  "sb_publishable_gbhc1x0xUwD6A8Xw126Wng_JPZuyecc";
+const SUPABASE_SERVICE_ROLE_KEY = process.env.VITE_SUPABASE_PUBLISHABLE_KEY;
 
 export const supabaseAdmin = createClient(
   SUPABASE_URL,
